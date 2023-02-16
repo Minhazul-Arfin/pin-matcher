c73dd030387fb65def3f47d216de6f31d79b3849
Minhazul-Arfin <arefinminhaz45@gmail.com>
2023-02-16 10:15:28 +0600

    user input part completed

--- js/pin-generator.js
+++ js/pin-generator.js
@@ -13,12 +13,36 @@ function generatePin(){
 
 
 
-
-
-
 document.getElementById('btn-pin-generate').addEventListener('click', function(){
     console.log("button clicked")
-    
-    console.log(generatePin())
+    const pin = generatePin();
+    const showPin =  document.getElementById('show-pin');
+    showPin.value = pin;
+})
 
+document.getElementById('calculator').addEventListener('click', function(event){
+    // CATCHING THE NUMBERS TYPED FROM CALCULATOR BUTTONS
+    const number = event.target.innerText;
+    console.log(number);
+
+    // GET PREVIOUS VALUE AND UPDATE IT
+    const typedNumberField = document.getElementById('user-input');
+    const previousTypedNumber = typedNumberField.value;
+
+    if(isNaN(number)){
+        if(number === 'C'){
+            typedNumberField.value = '';
+        }
+        else if (number === '<'){
+            const digits = previousTypedNumber.split('');
+            digits.pop();
+            const remainingDigits = digits.join('');
+            typedNumberField.value = remainingDigits;
+        }
+    }
+    else{
+        const newTypedNumber = previousTypedNumber + number;
+        typedNumberField.value = newTypedNumber;
+    }
+    
 })
